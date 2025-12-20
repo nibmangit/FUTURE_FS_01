cdb7a5653135df2c2470814c797ff06d7c4077c9
nibmangit <nibretumengaw@gmail.com>
2025-12-20 11:00:09 +0430

    final configuration for deployments

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k7nr90$x%3bj8*4%-f#h75eu#x+8l4==-w^)laxrf7@jdy&sp4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 
 # Application definition
